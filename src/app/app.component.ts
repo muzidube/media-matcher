bf7b530ba3cdbc01e2c79ad86855b2cc5aef175c
muzidube <muzidube1@gmail.com>
2024-11-11 01:15:44 +0000

    changing name and adding environment template file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ViewComponent } from './modules/home/components/view/view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ViewComponent],
+  imports: [ViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'thortful-genre-app';
+  title = 'media-matcher';
 }
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  tmdb_api_key: 'YOUR_TMDB_API_KEY',
+  igdb_api_key: 'YOUR_IGDB_API_KEY',
+};
